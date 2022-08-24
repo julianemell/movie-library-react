bcc2d346431ba53d7bf8243c7798674df925fe93
Julia Nemell <jnemell@gmail.com>
2022-08-24 13:01:44 +0200

    keepPreviousData added to useGenreMovies hook

--- src/hooks/useGenreMovies.js
+++ src/hooks/useGenreMovies.js
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query'
 import MovieAPI from '../services/MovieAPI'
 
 const useGenreMovies = (page, type) => {
-	return useQuery(['genre', page, type], MovieAPI.getGenre)
+	return useQuery(['genre', page, type], MovieAPI.getGenre, { keepPreviousData: true })
 }
 
 export default useGenreMovies
