2a560e80e2d5d92eb70e9436eca0861a5ca0c45f
Julia Nemell <jnemell@gmail.com>
2022-08-29 08:06:58 +0200

    clean up moviepage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,26 +1,23 @@
+import { Link } from 'react-router-dom'
+
 import usePopularMovies from '../hooks/usePopularMovies'
 import useLatestMovies from '../hooks/useLatestMovies'
 import useTopRatedMovies from '../hooks/useTopRatedMovies'
 
 import Container from 'react-bootstrap/Container'
-import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
 
 import LoadingSpinner from '../components/LoadingSpinner'
 import WarningAlert from '../components/alerts/WarningAlert'
-import ListMovies from '../components/ListMovies'
 import ScrollMovies from '../components/ScrollMovies'
 
 
 const MoviePage = () => {
 	const { data: popularMovies, isError: isErrorPopular, error: errorPopular, isLoading: isLoadingPopular } = usePopularMovies()
-	//console.log('popularMovies', popularMovies)
 
 	const { data: latestMovies, isError: isErrorLatest, error: errorLatest, isLoading: isLoadingLatest } = useLatestMovies()
-	//console.log('latestMovies', latestMovies)
 
 	const { data: topRatedMovies, isError: isErrorTopRated, error: errorTopRated, isLoading: isLoadingTopRated } = useTopRatedMovies()
-	//console.log('topRatedMovies', topRatedMovies)
 
 	return (
 		<Container className='my-4 p-0'>
