c33d7499a0c6e989de111eb469699686f1722feb
julianemell <jnemell@gmail.com>
2022-10-18 10:27:52 +0200

    added region to api requests

--- src/services/MovieAPI.js
+++ src/services/MovieAPI.js
@@ -4,7 +4,7 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 const language = 'en-US'
-const region = 'US'
+const region = 'SE'
 
 //gör en get-req för det den fick in i endpoint
 const get = async (endpoint) => {
@@ -14,7 +14,7 @@ const get = async (endpoint) => {
 
 
 const getPopularMovies = () => {
-	return get(`/movie/popular?api_key=${API_KEY}&include_adult=false&language=${language}`)
+	return get(`/movie/popular?api_key=${API_KEY}&include_adult=false&language=${language}&region=${region}`)
 }
 
 const getLatestMovies = () => {
@@ -22,24 +22,24 @@ const getLatestMovies = () => {
 }
 
 const getTopRatedMovies = () => {
-	return get(`/movie/top_rated?api_key=${API_KEY}&include_adult=false&language=${language}`)
+	return get(`/movie/top_rated?api_key=${API_KEY}&include_adult=false&language=${language}&region=${region}`)
 }
 
 const getMovie = (id) => {
-	return get(`/movie/${id}?api_key=${API_KEY}&include_adult=false&append_to_response=credits`)
+	return get(`/movie/${id}?api_key=${API_KEY}&include_adult=false&language=${language}&region=${region}&append_to_response=credits`)
 }
 
 const getPerson = (id) => {
-	return get(`/person/${id}?api_key=${API_KEY}&include_adult=false&append_to_response=movie_credits,image`)
+	return get(`/person/${id}?api_key=${API_KEY}&include_adult=false&language=${language}&region=${region}&append_to_response=movie_credits,image`)
 }
 
 const getGenre = ({ queryKey }) => {
 	const [_key, page, type] = queryKey
-	return get(`/discover/movie?api_key=${API_KEY}&include_adult=false&with_genres=${type}&page=${page}`)
+	return get(`/discover/movie?api_key=${API_KEY}&include_adult=false&language=${language}&region=${region}&with_genres=${type}&page=${page}`)
 }
 
 const getGenres = () => {
-	return get(`/genre/movie/list?api_key=${API_KEY}&include_adult=false&language=${language}`)
+	return get(`/genre/movie/list?api_key=${API_KEY}&include_adult=false&language=${language}&region=${region}`)
 }
 
 
